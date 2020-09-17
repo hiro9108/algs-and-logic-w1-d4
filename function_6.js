641cb5f587cee879df70c6208d202359599a1845
hiro9108 <hiroshi.8.egawa@gmail.com>
2020-09-17 11:08:59 -0700

    modify string

--- function_6.js
+++ function_6.js
@@ -1,6 +1,6 @@
 // Function 6:
 // even or odd?  will take in one parameter ( a number ) and print either even or odd, depending on the number. 
 
-const isEven = (even) => even % 2 === 0 ? `${even} is Odd` : `${even} is Even`; 
+const isEven = (even) => even % 2 === 0 ? `${even} is Even` : `${even} is Odd`; 
 
 console.log(isEven(24));
