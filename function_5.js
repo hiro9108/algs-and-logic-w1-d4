13af7e3ecad5f52aed5fa17642223bf1b9c60295
hiro9108 <hiroshi.8.egawa@gmail.com>
2020-09-17 11:10:55 -0700

    modify string

--- function_5.js
+++ function_5.js
@@ -1,6 +1,6 @@
 // Function 5:
 // return the area of a triangle when given two parameters, a Base width, and a height. 
 
-const areaTriangle = (height, width) => (height * width) /2;
+const areaTriangle = (height, width) => (height * width) / 2;
 
 console.log(areaTriangle(10, 2));
