4a86b2d19fd74826ad1398fbc067780f9adebd48
hiro9108 <hiroshi.8.egawa@gmail.com>
2020-09-18 08:14:23 -0700

    / => %

--- function_1.js
+++ function_1.js
@@ -1,6 +1,6 @@
-// Function 1: 
-// Return the remainder. Function will take two parameters. It will divide the first number by the second, and return the remainder. 
+// Function 1:
+// Return the remainder. Function will take two parameters. It will divide the first number by the second, and return the remainder.
 
-const remainder = (first, second) => first / second;
+const remainder = (first, second) => first % second;
 
 console.log((remainder(10, 2)));
